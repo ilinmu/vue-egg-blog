afb87e5af0d7dc588e58a0a07c4383e1ca6f852e
ilinmu <ilinmu@gmail.com>
2021-07-03 23:25:26 +0800

    refactor(package.json): add some modules

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'normalize.css/normalize.css';
+import '@ilinmu/atom/dist/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
