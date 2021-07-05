3bbc21775fe9dc80c6b3bb93dd5da05491d383c4
ilinmu <ilinmu@gmail.com>
2021-07-05 23:55:34 +0800

    feat(/login): edit login page,add input and button

--- client-react/src/views/login/index.tsx
+++ client-react/src/views/login/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Input,
+  Button,
+} from '@ilinmu/atom';
+
+import { ButtonType } from '@ilinmu/atom/dist/components/Button/button';
 
 function Login() {
-  return <h2>Login</h2>;
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+  const handleChangeAccount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAccount(event.target.value);
+  }
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const handleLogin = () => {
+    console.log('account', account);
+    console.log('password', password);
+  }
+  return (
+    <div className="login">
+      <Input placeholder="请输入账号" onChange={handleChangeAccount} value={account} />
+      <Input type="password" onChange={handleChangePassword} value={password} placeholder="请输入密码" />
+      <Button type={ButtonType.Primary} onClick={handleLogin}>登录</Button>
+    </div>
+  );
 }
 
 export default Login;
